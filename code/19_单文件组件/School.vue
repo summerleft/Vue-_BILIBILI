a882a1efb756a352e8b4aa906e4fbdf169d154d2
贺俊杰 <438058486@qq.com>
2021-07-22 15:26:47 +0800

    完成单文件组件部分学习

---
+++ code/19_单文件组件/School.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="demo">
+    <h2>学校名称:{{schoolName}}</h2>
+    <h2>学校地址:{{address}}</h2>
+    <student></student>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'School',
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style>
+  .demo{
+    background-color: orange;
+  }
+</style>
---
+++ code/19_单文件组件/Student.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="demo">
+    <h2>学生名称:{{name}}</h2>
+    <h2>学生年龄:{{age}}</h2>
+    <student></student>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+  data() {
+    return {
+      name: '张三',
+      age: 12
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style>
+  .demo{
+    background-color: orange;
+  }
+</style>
