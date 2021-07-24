614b3aae5c0aca9fe745423ebfdf1066b7c829f2
贺俊杰 <438058486@qq.com>
2021-07-24 17:59:24 +0800

    todolist init

---
+++ code/Chapter3-使用Vue脚手架/todolist/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+  import MyHeader from './components/MyHeader'
+  import MyList from './components/MyList'
+  import MyFooter from './components/MyFooter'
+
+  export default {
+    name: 'App',
+    components: {
+      MyHeader,
+      MyList,
+      MyFooter
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ code/Chapter3-使用Vue脚手架/todolist/src/components/MyList.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+  import MyItem from './MyItem'
+  
+  export default {
+    name: 'MyList',
+    components: {
+      MyItem
+    }
+  }
+</script>
+
+<style>
+
+</style>
