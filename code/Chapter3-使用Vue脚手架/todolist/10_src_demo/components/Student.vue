3fca622b0c80612b478266ac93c1fe8b3443c967
贺俊杰 <438058486@qq.com>
2021-07-27 11:15:39 +0800

    文件结构调整

---
+++ code/Chapter3-使用Vue脚手架/todolist/10_src_demo/components/Student.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="student">
+    <h2>学生姓名：{{name}}</h2>
+    <h2>学生性别：{{sex}}</h2>
+    <h2>当前求和为：{{number}}</h2>
+    <button @click="add">点我number++</button>
+    <button @click="sendStudentName">把学生名给App</button>
+    <button @click="unbind">解绑athust事件</button>
+    <button @click="death">销毁当前Student组件的实例(vc)</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Student',
+    data() {
+      return {
+        name: '张三',
+        sex: '男',
+        number: 0
+      }
+    },
+    methods: {
+      add() {
+        console.log('add回调被调用')
+        this.number++
+      },
+      sendStudentName() {
+        // 触发Student组件实例身上的事件
+        this.$emit('athust', this.name, 666, 888, 900)
+        // this.$emit('demo')
+      },
+      unbind() {
+        this.$off('athust') // 解绑一个自定义事件
+        // this.$off(['athust', 'demo']) // 解绑多个自定义事件
+        // this.$off() // 解绑所有自定义事件
+      },
+      death() {
+        this.$destroy() // 销毁当前student组件实例，销毁后所有Student实例的自定义事件全部不起作用
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .student {
+    background-color: pink;
+    padding: 5px;
+    margin-top: 30px;
+  }
+</style>
