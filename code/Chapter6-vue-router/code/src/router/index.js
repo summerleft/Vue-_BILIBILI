7cf75f6f09d24b246178410f39e70befb29df56b
贺俊杰 <438058486@qq.com>
2021-08-02 18:30:57 +0800

    vue-router开头

---
+++ code/Chapter6-vue-router/code/src/router/index.js
@@ -0,0 +1,17 @@
+// 该文件用于创建整个应用的路由器
+import VueRouter from 'vue-router'
+import About from '../components/About'
+import Home from '../components/Home'
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/about',
+      component: About
+    },
+    {
+      path: '/home',
+      component: Home
+    }
+  ]
+})
